81843ec14bf0fc3508213aa9fd3350a7d3d63cdb
Sushanth Upadhya K.N <sushanthupadhyakn@gmail.com>
2026-08-20 19:17:45 +0530

    Time: 15 ms (60.95%) | Memory: 48.1 MB (80.76%) - LeetSync

--- 503-next-greater-element-ii/next-greater-element-ii.java
+++ 503-next-greater-element-ii/next-greater-element-ii.java
@@ -2,7 +2,7 @@ class Solution {
     public int[] nextGreaterElements(int[] nums) {
         int n=nums.length;
         int[] res=new int[nums.length];
-        Deque<Integer>st=new ArrayDeque<>();
+        Stack<Integer>st=new Stack<>();
         Arrays.fill(res,-1);
         for(int i=2*n-1;i>=0;i--){
             int index=i%n;
