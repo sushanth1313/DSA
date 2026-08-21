ddeb1d597226827d18f56df241252fb725ba1f84
Sushanth Upadhya K.N <sushanthupadhyakn@gmail.com>
2026-08-21 18:39:23 +0530

    Time: 0 ms (100.00%) | Memory: 42.8 MB (84.42%) - LeetSync

---
+++ 58-length-of-last-word/length-of-last-word.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int lengthOfLastWord(String s) {
+    int n=s.length();
+    int i=n-1;
+    while(i>=0 && s.charAt(i)== ' '){
+        i--;
+    }
+    int count=0;
+      while(i>=0 && s.charAt(i)!= ' '){
+        count++;
+        i--;
+    }
+    return count;
+    }
+}
