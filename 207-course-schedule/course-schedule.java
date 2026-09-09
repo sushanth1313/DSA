db66d7241922185c9bf3945bfe235197fe6da0ad
Sushanth Upadhya K.N <sushanthupadhyakn@gmail.com>
2026-09-09 19:27:08 +0530

    Time: 7 ms (56.66%) | Memory: 46.8 MB (87.84%) - LeetSync

---
+++ 207-course-schedule/course-schedule.java
@@ -0,0 +1,34 @@
+class Solution {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        List<List<Integer>>graph=new ArrayList<>();
+        for(int i=0;i<numCourses;i++){
+            graph.add(new ArrayList<>());
+        }
+        int[] indegree=new int[numCourses];
+        for(int[] pre:prerequisites){
+            int course=pre[0];
+            int preq=pre[1];
+            graph.get(preq).add(course);
+            indegree[course]++;
+        }
+        Deque<Integer>q=new ArrayDeque<>();
+        for(int i=0;i<numCourses;i++){
+       
+            if(indegree[i]==0){
+                q.offer(i);
+            }
+        }
+        int count=0;
+        while(!q.isEmpty()){
+            int curr=q.poll();
+            count++;
+            for(int any:graph.get(curr)){
+                indegree[any]--;
+                if(indegree[any]==0){
+                    q.offer(any);
+                }
+            }
+        }
+        return count==numCourses;
+    }
+}
