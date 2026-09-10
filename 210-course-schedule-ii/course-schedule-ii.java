ad7e4628c8ecc7722d5fc6abaed5cd1a31f467cc
Sushanth Upadhya K.N <sushanthupadhyakn@gmail.com>
2026-09-10 19:32:45 +0530

    Time: 5 ms (80.22%) | Memory: 46.8 MB (52.60%) - LeetSync

---
+++ 210-course-schedule-ii/course-schedule-ii.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
+        List<List<Integer>>graph=new ArrayList<>();
+        int[] ans=new int[numCourses];
+        for(int i=0;i<numCourses;i++){
+            graph.add(new ArrayList<>());
+        }
+        int[] indegree=new int[numCourses];
+        for(int[] pre:prerequisites){
+            int course=pre[0];
+            int pq=pre[1];
+            graph.get(pq).add(course);
+            indegree[course]++;
+        }
+       
+        Deque<Integer>q=new ArrayDeque<>();
+        
+       for(int i=0;i<numCourses;i++){
+       if(indegree[i]==0){
+        q.offer(i);
+       }}
+       int index=0;
+       while(!q.isEmpty()){
+        int cur=q.poll();
+       ans[index++]=cur;
+       for(int any:graph.get(cur)){
+        indegree[any]--;
+        if(indegree[any]==0){
+            q.offer(any);
+        }
+       }
+       }
+       if(index!=numCourses){
+       return new int[0];
+       }
+       
+     return ans;
+       }   
+   
+}
